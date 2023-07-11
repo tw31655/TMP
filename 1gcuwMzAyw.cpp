00d7091b058e418dce89f3c11f07aff266a3d04c
Viktor Matviinko <139056095+tw31655@users.noreply.github.com>
2023-07-11 19:57:47 +0200

    Add files via upload

---
+++ 1gcuwMzAyw.cpp
@@ -0,0 +1,8 @@
+/* This is a C++ program */
+
+#include <iostream>
+
+int main() {
+    std::cout << 'Hello, World!' << std::endl;
+    return 0;
+}
---
+++ 8vT2nCMX0L.py
@@ -0,0 +1,7 @@
+/* This is a CSS file */
+
+body {
+    background-color: #f0f0f0;
+    color: #333;
+    font-family: Arial, sans-serif;
+}
